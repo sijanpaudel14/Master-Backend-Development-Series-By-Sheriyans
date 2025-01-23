2b10c0eb7135314d175924c0f48f6c9aa2499e34
Sijan Paudel <sijanpaudel44@gmail.com>
2025-01-23 22:48:53 +0545

    Learned how to set and read cookies

---
+++ Practise Backend/JWT Authentication/app.js
@@ -0,0 +1,16 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+app.use(cookieParser());
+
+app.get("/", function(req, res){
+    res.cookie("name", "harsh");
+    res.send("home");
+})
+
+app.get("/read", function(req, res){
+    console.log(req.cookies);
+    res.send("read page");
+})
+
+app.listen(3000);
