6ffba5db02effecf645f80b37e2a3c665f830251
Sijan Paudel <sijanpaudel44@gmail.com>
2025-01-23 23:15:03 +0545

    Learned how to check encrypted password

--- Practise Backend/JWT Authentication/app.js
+++ Practise Backend/JWT Authentication/app.js
@@ -4,12 +4,10 @@ const app = express();
 const bcrypt = require('bcrypt');
 
 app.get("/", function(req, res){
-    bcrypt.genSalt(10, function(err, salt){
-        bcrypt.hash("pololololo", salt, function(err, hash){
-            // Store hash in your password DB
-            console.log(hash);
-        });
-    });
+    bcrypt.compare("pololololo", '$2b$10$effxT9Dveskk6ottTEUDau/7GQZ97ILg/wbP1R.80axxRduwvRune', function(err, result){
+            console.log(result);
+            
+    })
 })
 
 app.listen(3000);
